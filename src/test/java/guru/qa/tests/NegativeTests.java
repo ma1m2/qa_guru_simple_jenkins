1f0bf5267bc07874fde22e1d0f7475755195503e
Sveta <ma1m2@mail.ru>
2026-03-26 15:30:14 +0300

    add different tests

---
+++ src/test/java/guru/qa/tests/NegativeTests.java
@@ -0,0 +1,57 @@
+package guru.qa.tests;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class NegativeTests {
+  
+  @Test
+  public void negativeTest0() {
+    Assertions.assertTrue(false);
+  }
+
+  @Test
+  public void negativeTest1() {
+    Assertions.assertTrue(false);
+  }
+
+  @Test
+  public void negativeTest2() {
+    Assertions.assertTrue(false);
+  }
+
+  @Test
+  public void negativeTest3() {
+    Assertions.assertTrue(false);
+  }
+
+  @Test
+  public void negativeTest4() {
+    Assertions.assertTrue(false);
+  }
+
+  @Test
+  public void negativeTest5() {
+    Assertions.assertTrue(false);
+  }
+
+  @Test
+  public void negativeTest6() {
+    Assertions.assertTrue(false);
+  }
+
+  @Test
+  public void negativeTest7() {
+    Assertions.assertTrue(false);
+  }
+
+  @Test
+  public void negativeTest8() {
+    Assertions.assertTrue(false);
+  }
+
+  @Test
+  public void negativeTest9() {
+    Assertions.assertTrue(false);
+  }
+}
---
+++ src/test/java/guru/qa/tests/SkippedTests.java
@@ -0,0 +1,27 @@
+package guru.qa.tests;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+public class SkippedTests {
+
+  @Test
+  @Disabled
+  public void positiveTest0() {
+    Assertions.assertTrue(true);
+  }
+
+  @Test
+  @Disabled
+  public void positiveTest1() {
+    Assertions.assertTrue(true);
+  }
+
+  @Test
+  @Disabled
+  public void positiveTest2() {
+    Assertions.assertTrue(true);
+  }
+
+}
